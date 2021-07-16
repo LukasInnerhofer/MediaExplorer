ee8bde4e641a9bc13ba6ec66e21b86d18a5ba1a6
LukasInnerhofer <lukas.ihofer@gmail.com>
2021-07-16 12:26:21 +0200

    Initial commit

---
+++ MediaExplorer.Wpf/Views/ChooseProfileView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MvvmCross.Platforms.Wpf.Views;
+using MvvmCross.ViewModels;
+using MediaExplorer.Core.ViewModels;
+
+namespace MediaExplorer.Wpf.Views
+{
+    /// <summary>
+    /// Interaction logic for ChooseProfileView.xaml
+    /// </summary>
+    [MvxViewFor(typeof(ChooseProfileViewModel))]
+    public partial class ChooseProfileView : MvxWpfView
+    {
+        public ChooseProfileView()
+        {
+            InitializeComponent();
+        }
+    }
+}
