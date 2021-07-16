c3758beeff764e0f126f76779d75da4cf20959d5
LukasInnerhofer <lukas.ihofer@gmail.com>
2021-07-16 12:35:50 +0200

    Initial commit

---
+++ MediaExplorer.Wpf/Services/FileSystemService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediaExplorer.Core.Services;
+
+namespace MediaExplorer.Wpf.Services
+{
+    class FileSystemService : IFileSystemService
+    {
+        public Task CreateFileAsync(string path)
+        {
+            File.Create(path).Close();
+            return Task.CompletedTask;
+        }
+
+        public Task<bool> FileExistsAsync(string path)
+        {
+            return Task.FromResult(File.Exists(path));
+        }
+
+        public Task<FileStream> OpenFileAsync(string path)
+        {
+            return Task.FromResult(new FileStream(path, FileMode.Open));
+        }
+    }
+}
